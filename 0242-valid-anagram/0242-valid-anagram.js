5cb0920abb29b6c80c19277ab6dc625a4bb2605b
Peter <133621347+kentr9989@users.noreply.github.com>
2024-07-18 22:24:49 +1000

    Time: 77 ms (38.79%), Space: 52.2 MB (62.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,17 +3,38 @@
  * @param {string} t
  * @return {boolean}
  */
-let isAnagram = (s,t) => {
-    if(s.length !== t.length) return false;
-    
-    let copyS = [...s];
-    repeatIndex = [];
-    
-    for(const letter of t) {
-        index = copyS.indexOf(letter);
-        if(index === -1) return false;
-        copyS.splice(index,1);
+var isAnagram = function(s, t) {
+    // if length s not equal t return false
+    if(s.length !== t.length) {
+        return false;
     }
-    return true;
+    
+    // make a hashmap to store character 
+    let charCount = new Map();
 
-}
+    // store hashmap with char of s
+    for(const char of s) {
+        charCount.set(char , (charCount.get(char) || 0) + 1 );
+    }
+    
+    // for char of t check following
+    // if the hash map does not contain char return false
+    // decrease the count of the hashmap
+    // if the hash map contain then if count < 0 return false
+    
+    for(const char of t) {
+        if(!charCount.has(char)) {
+            return false;
+        }
+        charCount.set(char,charCount.get(char) - 1);
+        if(charCount.get(char) < 0) {
+            return false;
+        }
+    }
+    
+    return true;
+    // return true because the hashmap contain both s and t
+    
+    // Time complexity : O(n)
+    // Space complexity : O(1) because of 26 English characters
+};
