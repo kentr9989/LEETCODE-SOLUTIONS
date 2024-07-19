e2e603b5bee90077c0a86caa16da2400d45e7507
Peter <133621347+kentr9989@users.noreply.github.com>
2024-07-19 13:17:25 +1000

    Time: 95 ms (96.11%), Space: 64.1 MB (19.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,27 +2,30 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-let groupAnagrams = (strs) => {
-  let newArr = {};
-  let resArr = [];
-  for (const str of strs) {
-    splitStr = str.split("");
-
-    sortSplitStr = splitStr.sort().join();
-
-    if (newArr[sortSplitStr] === undefined) {
-      newArr[sortSplitStr] = [str];
-      // newArr.sortSplitStr.push(str);
-    } else {
-      newArr[sortSplitStr].push(str);
+var groupAnagrams = function(strs) {
+    // Create a new hashmap {sortedStr, [str]}
+    let map = new Map();
+    
+    // Loop through strs
+    // For each str, create sortedStr 
+    // Check if sortedStr exist in map
+    // If yes then push into array
+    // If not then create new array
+    for(let i = 0 ; i < strs.length; i++) {
+        // Split to array of single character then sort and join
+        let sortedStr = strs[i].split('').sort().join('');
+        if(!map.has(sortedStr)) {
+            map.set(sortedStr,[]);
+        } 
+        
+        map.get(sortedStr).push(strs[i]);
+        
     }
-  }
-  // console.log(newArr);
-  Object.entries(newArr).forEach(([key, value]) => {
-    let subNewArr = [];
-    value.forEach((v) => subNewArr.push(v));
-    resArr.push(subNewArr);
-  });
-  return resArr;
-  // console.log(resArr);
+    
+    // Return array from values of hashmap
+    // console.log(map);
+    return Array.from(map.values());
+    
+    // Time complexity : O(n * klogk) where k is maximum number for each       string
+    // Space complexity: O(n*k)
 };
