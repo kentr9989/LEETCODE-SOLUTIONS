30f6682b9d9c7eb814da03854500c5049aee655e
Peter <133621347+kentr9989@users.noreply.github.com>
2024-07-19 22:26:14 +1000

    Time: 104 ms (56.41%), Space: 62.2 MB (81.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,70 +2,28 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-let productExceptSelf = (nums) => {
-  //[x,y,z]
-  let answerArr = [];
-
-  
-  let rightProduct = 1;
-  let rightProductArr = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (i + 1 < nums.length) {
-      rightProductArr[i] = 1 * nums[i + 1];
-    } else if (i === nums.length - 1) {
-      rightProductArr[i] = 1;
+var productExceptSelf = function(nums) {
+    
+    // Create an array of answer
+    let answer = new Array(nums.length).fill(1);
+    
+    // Compute the prefix (from 0 -> nums.length - 1)
+    // Start with prefix = 1 go from left -> right
+    // answer[i] *= prefix then prefix *= nums[i]
+    let prefix = 1;
+    for(let i = 0; i < nums.length; i++) {
+        answer[i] *= prefix;
+        prefix *= nums[i];
     }
-  }
-
-  for (let k = rightProductArr.length - 1; k >= 0; k--) {
-    if (k === rightProductArr.length - 1) {
-      rightProductArr[k] = rightProductArr[k] * rightProduct;
-    } else {
-      rightProductArr[k] = rightProductArr[k + 1] * rightProductArr[k];
+    
+    // Compute the suffix (from nums.length - 1 -> 0)
+    // Start with suffix = 1 go from right -> left
+    // answer[i] *= suffix then suffix *= nums[i]
+    let suffix = 1;
+    for(let i = nums.length - 1; i >= 0; i--) {
+        answer[i] *= suffix;
+        suffix *= nums[i];
     }
-  }
-  // left 1st
-  let leftProductArr = [];
-  for (let i = nums.length - 1; i >= 0; i--) {
-    if (i === 0) {
-      leftProductArr[i] = 1;
-    } else if (i - 1 >= 0) {
-      leftProductArr[i] = 1 * nums[i - 1];
-    }
-  }
-  // console.log(leftProductArr);
-  // left 2nd
-  for (let i = 0; i < nums.length; i++) {
-    if (i === 0) {
-      leftProductArr[i] = leftProductArr[i];
-    } else {
-      leftProductArr[i] = leftProductArr[i - 1] * leftProductArr[i];
-    }
-  }
-
-  for (const i in nums) {
-    if (leftProductArr[i] * rightProductArr[i] === -0) answerArr[i] = 0;
-    else {
-      answerArr[i] = leftProductArr[i] * rightProductArr[i];
-    }
-    // answerArr[i] = leftProductArr[i] * rightProductArr[i];
-  }
-  // console.log(answerArr);
-  return answerArr
+    
+    return answer;
 };
-
-// productExceptSelf([-1, 1, 0, -3, 3]);
-//logic
-// [1,2,3,4]
-//right
-// [1*2,1*3,1*4,1] = [2,3,4,1]
-// want it to be then go left
-// [1*2*3*4,1*3*4,1*4*1,1]
-// [24, 12, 4 , 1]
-
-// [1,2,3,4]
-// left
-// [1,1*1,1*2,1*3] = [1,1,2,3]
-// [1,1*1*1,1*2*1,1*3*2]
-// [1,1,2,6]
-// => [24,12,8,6]
