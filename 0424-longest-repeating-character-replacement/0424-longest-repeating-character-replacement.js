9df4899a0512380a72f9520b2e273e70068f891f
Peter <133621347+kentr9989@users.noreply.github.com>
2024-07-21 23:41:06 +1000

    Time: 79 ms (42.18%), Space: 50.9 MB (45.40%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    // Init countMap to store character : its occurence
+    // Init left equal 0
+    // Init res equal 0
+    let countMap = new Map();
+    let left = 0;
+    let res = 0;
+    let maxF = 0;
+    
+    // Exp: Here we will make a sliding window
+    // with left and right = 0. If the current
+    // length of window - maximum occurence of character
+    // is greater than k , then we delete the occurence 
+    // of left character from hashmap and increase left pointer
+    for(let right = 0; right < s.length; right++) {
+        
+        countMap.set(s[right], (countMap.get(s[right]) || 0) + 1);
+        maxF = Math.max(maxF,countMap.get(s[right]))
+        while( (right - left + 1) - maxF > k ) {
+            countMap.set(s[left], countMap.get(s[left]) - 1);
+            left++;
+        }
+        
+        res = Math.max(res, right-left+1);
+        
+    }
+    
+    // return res
+    return res;
+    
+    // Time complexity : O(26n) because need to check max-occurence for every 26 letters
+    // Space complexity : O(26) = O(1) because need to store at most 26 characters
+};
