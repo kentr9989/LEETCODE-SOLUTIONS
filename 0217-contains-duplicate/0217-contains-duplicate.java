6d6f2e9652f6ebb8eb225d7e91f6fe18b24114c4
Peter <133621347+kentr9989@users.noreply.github.com>
2024-08-13 13:23:00 +1000

    Time: 11 ms (71.10%), Space: 57.9 MB (60.70%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,18 +1,14 @@
-import java.util.*;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       Map<Integer,Integer> checkDuplicate = new HashMap<>();
-       for(int num : nums) {
-           if(checkDuplicate.get(num) == null) {
-               checkDuplicate.put(num,1);
-           } else {
-               int value = checkDuplicate.get(num);
-               checkDuplicate.replace(num,value + 1);
-               return true;
-           }
-       }
-       return false;
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true;
+            }
+            map.put(num,1);
+        }
+        
+        return false;
     }
 }
-
