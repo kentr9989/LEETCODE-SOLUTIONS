adaaa13f22c7212f7b4cb0c6ab5ab0278409f8c7
Peter <133621347+kentr9989@users.noreply.github.com>
2024-08-20 20:54:51 +1000

    Time: 738 ms (20.71%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,17 +1,17 @@
+-- Create VIEW for first order
+
 WITH FirstOrders AS (
-    SELECT
-        customer_id,
-        MIN(order_date) AS first_order_date
-    FROM
-        Delivery
-    GROUP BY
-        customer_id
+    SELECT customer_id,
+           MIN(order_date) AS first_order_date 
+    FROM Delivery
+    GROUP BY customer_id
 )
-SELECT
-    ROUND(SUM(CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS immediate_percentage
-FROM
-    Delivery d
-JOIN
-    FirstOrders f
-ON
-    d.customer_id = f.customer_id AND d.order_date = f.first_order_date;
+
+SELECT 
+    ROUND(SUM(CASE WHEN d.customer_pref_delivery_date = d.order_date THEN 1 ELSE 0 END) * 100
+    / COUNT(*),2)  
+    AS immediate_percentage
+FROM Delivery d
+JOIN FirstOrders f
+ON f.customer_id = d.customer_id AND 
+   f.first_order_date = d.order_date;
