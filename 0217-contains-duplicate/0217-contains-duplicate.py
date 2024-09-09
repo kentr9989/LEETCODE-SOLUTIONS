a3b6d97dc3f5595017e486399b33f921b60f8252
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-10 09:44:24 +1000

    Time: 416 ms (89.31%), Space: 26 MB (75.20%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,13 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        num_map = {}
+        # Space complexity : O(size of nums)
+        # Time complexity : O(size of nums)
+        freq_count = {}
         for num in nums:
-            if num in num_map: 
+            if num in freq_count:
                 return True
-            num_map[num] = True
-            
+            freq_count[num] = True
         return False
+        
+
     
     
         
