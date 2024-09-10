7b85b57fb682768c5d11e77dda889bd87f9266f8
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-10 18:23:37 +1000

    Time: 4232 ms (22.32%), Space: 31.8 MB (82.33%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -2,26 +2,51 @@ class Solution:
     # Time complexity : O(n) - max visit for each number is twice
     # Space complexity : O(n)
     def longestConsecutive(self, nums: List[int]) -> int:
-        # Create new set
-        num_set = set(nums)
-        # Init longest_streak
+          # Create new set of nums
+        num_set = set(nums)  
+            
+          # create longest_streak
         longest_streak = 0
-        
-        # Loop through each num in nums :
-        #   check if it not the start of sequence (num - 1) not in set:
-        #       initialise length equal 0
-        #       while (n + length) in the set:
-        #           increase length
-        #       update the longest_streak
+            
+          # for each num in nums:
+          #     check if num - 1 not in set (mean it is start of list):
+          #         create length = 0
+          #         while num + length in set
+          #             increase the length
+          #  
         for num in nums:
-            if (num - 1) not in num_set: #start of sequence
+            if (num-1) not in num_set:
                 length = 0
-                while (num+length) in num_set:
+                while (num + length) in num_set:
                     length += 1
-                longest_streak = max(length,longest_streak)
-        
-        # return longest_streak
+                longest_streak = max(length, longest_streak)
         return longest_streak
+        
+        
+#----------------------------------------------------------------
+
+
+
+#         # Create new set
+#         num_set = set(nums)
+#         # Init longest_streak
+#         longest_streak = 0
+        
+#         # Loop through each num in nums :
+#         #   check if it not the start of sequence (num - 1) not in set:
+#         #       initialise length equal 0
+#         #       while (n + length) in the set:
+#         #           increase length
+#         #       update the longest_streak
+#         for num in nums:
+#             if (num - 1) not in num_set: #start of sequence
+#                 length = 0
+#                 while (num+length) in num_set:
+#                     length += 1
+#                 longest_streak = max(length,longest_streak)
+        
+#         # return longest_streak
+#         return longest_streak
     
     
 # Test case 1: Regular case
