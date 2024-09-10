9fa6dcddc36f287e0aaab8293258bf18c4dc5363
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-10 10:14:55 +1000

    Time: 55 ms (26.08%), Space: 16.8 MB (92.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,32 +1,34 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # if length s not equal t return false
+class Solution:
+    # Time complexity: O(length of s + length of t) 
+    # Space complexity: O(length of s)
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if the length of s not equal length of t:
+        #   return False
         if len(s) != len(t):
-            return False;
-        
-        # make a dictionary to store character counts
-        firstMapCount = {}
-
-        # store dictionary with char counts from s
+            return False
+        # Create the map to count the freq_count_s of s
+        freq_count_s = {}
+        # Map all the character of s to freq_count_s
         for char in s:
-            firstMapCount[char] = firstMapCount.get(char,0) + 1
-        
-        
-        # for each char in t check the following
-        for char in t:
-            if char not in firstMapCount:
-                return False
+            freq_count_s[char] = freq_count_s.get(char,0) + 1
             
-            firstMapCount[char] -= 1
-            if firstMapCount.get(char) == 0:
-                del firstMapCount[char]
+        # For each character char of t
+        # If char is not in freq_count_s:
+        #   return False
+        # reduce the freq_count_s[char] by 1
+        # if freq_count_s[char] is equal 0:
+        #   delete freq_count_s[char]
+        for char in t:
+            if char not in freq_count_s:
+                return False
+            freq_count_s[char] -= 1
+            if freq_count_s[char] == 0:
+                del freq_count_s[char]
         
-        # return true because the dictionary contains both s and t
-        return len(firstMapCount) == 0
+        
+        # return true if length of freq_count_s is 0 else false
+        return len(freq_count_s) == 0
+        
+        
         
         
