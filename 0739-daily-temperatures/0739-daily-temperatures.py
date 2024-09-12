51787a2677d06adb8a2816836e9e4f54edeee71a
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-12 16:41:16 +1000

    Time: 949 ms (8.07%), Space: 32.1 MB (19.17%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,21 +2,32 @@ class Solution:
     # Time complexity : O(n)
     # Space complexity : O(n)
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # init res array
         res = [0] * len(temperatures)
-        # init stack of [temp,index]
+        
         stack = []
-        # iterate index, temp of temperatures:
-        #   while stack is not empty and temp > temp top of stack:
-        #       pop from our stack
-        #       add to res array
-        #   append current index and temp to stack
+        
         for index,temp in enumerate(temperatures):
-            while stack and temp > stack[-1][0]:
-                stack_temp, stack_index = stack.pop()
-                print(stack_index)
+            while stack and temp > stack[-1][1]:
+                stack_index,stack_temp = stack.pop()
                 res[stack_index] = index - stack_index
-            stack.append([temp,index])
-        # return res 
-        # no need to run through res to assign 0 because initially we alrd did
+            stack.append([index,temp])
+        
         return res
+        # # init res array
+        # res = [0] * len(temperatures)
+        # # init stack of [temp,index]
+        # stack = []
+        # # iterate index, temp of temperatures:
+        # #   while stack is not empty and temp > temp top of stack:
+        # #       pop from our stack
+        # #       add to res array
+        # #   append current index and temp to stack
+        # for index,temp in enumerate(temperatures):
+        #     while stack and temp > stack[-1][0]:
+        #         stack_temp, stack_index = stack.pop()
+        #         print(stack_index)
+        #         res[stack_index] = index - stack_index
+        #     stack.append([temp,index])
+        # # return res 
+        # # no need to run through res to assign 0 because initially we alrd did
+        # return res
