b71970f74e251ca8e2497d21fbdf5c8483e4afe4
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-15 12:34:42 +1000

    Time: 265 ms (59.90%), Space: 18.2 MB (13.39%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -2,34 +2,53 @@ class Solution:
     # Time complexity: O(log(max(piles)*piles))
     # Space complexity: O(max(piles))
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # left is equal 1 (bc need to eat least 1 banana)
         left = 1
-        # right is equal max(piles) - last index
-        right = max(piles) # piles[-1]
-        # res = right
+        right = max(piles)
         res = right
-        # while left is less than or qual right:
-        #   mid = left + right / 2
-        #   hours = 0
-        #   go to every pile:
-        #       hours += math.ceil(pile/mid)
         
-        #   if hours <= h:
-        #       update res
-        #       right = mid - 1 (search left portion)
-        #   else:
-        #       left = mid + 1 (search for right portion)
         while left <= right:
             mid = (left + right) // 2
             hours = 0
             for pile in piles:
                 hours += math.ceil(pile/mid)
-            
             if hours <= h:
-                res = min(mid,res)
+                res = mid
                 right = mid - 1
             else:
                 left = mid + 1
         
-        # return res
         return res
+        
+        
+        
+#         # left is equal 1 (bc need to eat least 1 banana)
+#         left = 1
+#         # right is equal max(piles) - last index
+#         right = max(piles) # piles[-1]
+#         # res = right
+#         res = right
+#         # while left is less than or qual right:
+#         #   mid = left + right / 2
+#         #   hours = 0
+#         #   go to every pile:
+#         #       hours += math.ceil(pile/mid)
+        
+#         #   if hours (mid) <= h (target):
+#         #       update res
+#         #       right = mid - 1 (search left portion)
+#         #   else:
+#         #       left = mid + 1 (search for right portion)
+#         while left <= right:
+#             mid = (left + right) // 2
+#             hours = 0
+#             for pile in piles:
+#                 hours += math.ceil(pile/mid)
+            
+#             if hours <= h:
+#                 res = min(mid,res)
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+        
+#         # return res
+#         return res
