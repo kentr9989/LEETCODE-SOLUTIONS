27b752a4f6bb80112159110c11693a7315a1e47e
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-15 16:44:02 +1000

    Time: 54 ms (61.19%), Space: 16.5 MB (74.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,20 +6,43 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         new_head = ListNode()
-        curr_new_head = new_head 
-        remainder = 0 
+        
+        tail = new_head
+        remainder = 0
         while l1 or l2 or remainder:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
             
             val = v1 + v2 + remainder
-            print("remainder" + str(remainder))
-            remainder = val // 10
+            remainder =  val // 10
             val = val % 10
-            curr_new_head.next = ListNode(val)
-            curr_new_head = curr_new_head.next
+            new_node = ListNode(val,None)
+            tail.next = new_node
+            tail = new_node
             
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
+        
         return new_head.next
+        
+        
+        
+        
+        
+#         new_head = ListNode()
+#         curr_new_head = new_head 
+#         remainder = 0 
+#         while l1 or l2 or remainder:
+#             v1 = l1.val if l1 else 0
+#             v2 = l2.val if l2 else 0
+            
+#             val = v1 + v2 + remainder
+#             remainder = val // 10
+#             val = val % 10
+#             curr_new_head.next = ListNode(val)
+#             curr_new_head = curr_new_head.next
+            
+#             l1 = l1.next if l1 else None
+#             l2 = l2.next if l2 else None
+#         return new_head.next
             
