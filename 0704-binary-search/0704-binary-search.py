a048d3814b119c5b6e86c05fddb151c1f05a67fd
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-15 11:51:22 +1000

    Time: 203 ms (5.99%), Space: 18.1 MB (69.81%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,25 +1,36 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # init left 0
-        left = 0
-        # init right last index
-        right = len(nums) - 1
-        # while left less than or equal to right:
-        #   mid equal to (left + right) // 2
-        #   if the value of mid index > target:
-        #       decrease the right to mid - 1
-        #   else if value of mid index < target:
-        #       increase left to mid + 1
-        #   else:
-        #       return m
+        left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] < target:
-                left = mid + 1
+                left += 1
             elif nums[mid] > target:
-                right = mid - 1
-            else:
+                right -= 1
+            else: 
                 return mid
-        
-        # return -1 - means not found
         return -1
+        
+#         # init left 0
+#         left = 0
+#         # init right last index
+#         right = len(nums) - 1
+#         # while left less than or equal to right:
+#         #   mid equal to (left + right) // 2
+#         #   if the value of mid index > target:
+#         #       decrease the right to mid - 1
+#         #   else if value of mid index < target:
+#         #       increase left to mid + 1
+#         #   else:
+#         #       return m
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if nums[mid] < target:
+#                 left = mid + 1
+#             elif nums[mid] > target:
+#                 right = mid - 1
+#             else:
+#                 return mid
+        
+#         # return -1 - means not found
+#         return -1
