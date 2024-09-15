c5433f029e61371c70eb5cad36209105ec50b24a
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-15 15:42:10 +1000

    Time: 36 ms (59.65%), Space: 16.5 MB (68.30%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,25 +5,35 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # create dummy node to head
         dummy = ListNode(0,head)
-        # init left pointer to dummy
-        left = dummy
-        # init right pointer to head + n (use loop)
-        right = head
+        left,right = dummy, head
         while n > 0 and right:
             right = right.next
             n -= 1
-        # while right is not null:
-        #   shift left = left.next
-        #   right = right.next
         while right:
             left = left.next
             right = right.next
-            
-        # delete the node
-        # left.next = left.next.next
         left.next = left.next.next
-        
-        # return dummy.next
         return dummy.next
+#         # create dummy node to head
+#         dummy = ListNode(0,head)
+#         # init left pointer to dummy
+#         left = dummy
+#         # init right pointer to head + n (use loop)
+#         right = head
+#         while n > 0 and right:
+#             right = right.next
+#             n -= 1
+#         # while right is not null:
+#         #   shift left = left.next
+#         #   right = right.next
+#         while right:
+#             left = left.next
+#             right = right.next
+            
+#         # delete the node
+#         # left.next = left.next.next
+#         left.next = left.next.next
+        
+#         # return dummy.next
+#         return dummy.next
