df67df59408405cf85641eb08428a9f560d154aa
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-15 13:11:59 +1000

    Time: 28 ms (95.94%), Space: 17.7 MB (26.59%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,21 +5,41 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Iterative sol
-        # [A]<-[B]<-[C]
-        # Init prev as NULL
-        prev = None
-        # Init curr as head
-        curr = head
-        # while curr is not NULL:
-        #   store temp node => ([B])
-        #   curr->next = prev  =>  (NULL<-[A])
-        #   prev = curr => (prev = [A])
-        #   curr = temp node => ([B])
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+        # Recursive sol
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+        # Base case: If head is null or head->next is null:
+        #   return head
+        # Recursively the rest of the list and assign to new head
+        # head->next->next = head
+        # head->next = null
+        # return new head
+        if not head or not head.next:
+            return head
+        
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        
+        return newHead
+        
+        # # Iterative sol
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        # # [A]<-[B]<-[C]
+        # # Init prev as NULL
+        # prev = None
+        # # Init curr as head
+        # curr = head
+        # # while curr is not NULL:
+        # #   store temp node => ([B])
+        # #   curr->next = prev  =>  (NULL<-[A])
+        # #   prev = curr => (prev = [A])
+        # #   curr = temp node => ([B])
+        # while curr:
+        #     temp = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = temp
+        # # return prev
         # return prev
-        return prev
