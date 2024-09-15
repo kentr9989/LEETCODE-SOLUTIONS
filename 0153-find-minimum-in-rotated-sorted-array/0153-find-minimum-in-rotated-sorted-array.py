7d9b69d6e8d661d25fd24f40dbeeca4e18666bae
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-15 12:56:53 +1000

    Time: 41 ms (69.68%), Space: 16.9 MB (45.23%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -2,35 +2,56 @@ class Solution:
     # Time complexity: O(logn)
     # Space complexity: O(1)
     def findMin(self, nums: List[int]) -> int:
-        # init left pointer first index of num
-        left = 0
-        # init right pointer last index of num
-        right = len(nums) -1
-        # init res = first element of nums
+        left, right = 0, len(nums) - 1
         res = nums[0]
-        # while left less than or equal to right pointer:
-        #   if nums at left < nums at right (in sort section):
-        #       update the res with nums at left
-        #       break for loop
-        #   calculate mid posititon
-        #   update the res with nums at mid
-        #   if nums at mid >= nums at left:
-        #       move the left pointer to mid + 1
-        #   else:
-        #       move the right pointer to mid - 1
-        
-        # return res
         
         while left <= right:
             if nums[left] < nums[right]:
                 res = min(res,nums[left])
                 break
+                
             mid = (left + right) // 2
-            print(mid)
-            res = min(res,nums[mid])
-            
+            print("mid" + str(mid))
+            res = min(nums[mid],res)
             if nums[mid] >= nums[left]:
                 left = mid + 1
             else:
                 right = mid - 1
         return res
+        
+        
+        
+        
+        
+        
+#         # init left pointer first index of num
+#         left = 0
+#         # init right pointer last index of num
+#         right = len(nums) -1
+#         # init res = first element of nums
+#         res = nums[0]
+#         # while left less than or equal to right pointer:
+#         #   if nums at left < nums at right (in sort section):
+#         #       update the res with nums at left
+#         #       break for loop
+#         #   calculate mid posititon
+#         #   update the res with nums at mid
+#         #   if nums at mid >= nums at left:
+#         #       move the left pointer to mid + 1
+#         #   else:
+#         #       move the right pointer to mid - 1
+        
+#         # return res
+        
+#         while left <= right:
+#             if nums[left] < nums[right]:
+#                 res = min(res,nums[left])
+#                 break
+#             mid = (left + right) // 2
+#             res = min(res,nums[mid])
+            
+#             if nums[mid] >= nums[left]:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+#         return res
