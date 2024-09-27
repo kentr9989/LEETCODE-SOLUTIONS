531b18cfb023c581020143f8e7795f52330f17f0
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-27 21:10:51 +1000

    Time: 955 ms (5.05%), Space: 27.9 MB (5.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,63 @@
 class Solution:
-    # Time complexity: 
+    # Time complexity: O(n)
+    # Space complexity: O(1)
     def maxProfit(self, prices: List[int]) -> int:
-        # init left pointer 0 (Buy)
-        left = 0
-        # init right pointer 1 (Sell)
-        right = 1
-        # init max_profit = 0
         max_profit = 0
-        
-        # while right < length of prices
-        #   if price[left] < price[right]:
-        #       calc profit
-        #       update the max_profit
-        #   else:
-        #       update the left to the right
-        #   update the right pointer by 1
+        left , right = 0, 1
         while right < len(prices):
             if prices[left] < prices[right]:
-                profit = prices[right] - prices[left]
-                max_profit = max(max_profit,profit)
+                curr_profit = prices[right] - prices[left]
+                print(f"curr profit: {curr_profit}")
+                max_profit = max(max_profit, curr_profit)
             else:
                 left = right
             right += 1
         
-        #  return max_profit
+        
         return max_profit
+
+        
+        
+        
+        
+        
+        
+        
+        # left = 0
+        # right = 1
+        # max_profit = 0
+        # while right < len(prices):
+        #     if prices[left] < prices[right]:
+        #         profit = prices[right] - prices[left]
+        #         max_profit = max(max_profit,profit)
+        #     else:
+        #         left = right
+        #     right += 1
+        # return max_profit
+            
+        
+        
+#         # init left pointer 0 (Buy)
+#         left = 0
+#         # init right pointer 1 (Sell)
+#         right = 1
+#         # init max_profit = 0
+#         max_profit = 0
+        
+#         # while right < length of prices
+#         #   if price[left] < price[right]:
+#         #       calc profit
+#         #       update the max_profit
+#         #   else:
+#         #       update the left to the right
+#         #   update the right pointer by 1
+#         while right < len(prices):
+#             if prices[left] < prices[right]:
+#                 profit = prices[right] - prices[left]
+#                 max_profit = max(max_profit,profit)
+#             else:
+#                 left = right
+#             right += 1
+        
+#         #  return max_profit
+#         return max_profit
