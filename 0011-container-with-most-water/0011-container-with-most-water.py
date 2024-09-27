0229860b1929c6bffb506e2aacfbffac6604175c
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-27 18:15:14 +1000

    Time: 692 ms (5.06%), Space: 30 MB (5.83%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -2,19 +2,33 @@ class Solution:
     # Time complexity : O(n)
     # Space complexity: O(1)
     def maxArea(self, height: List[int]) -> int:
-        res = 0  
-        left, right = 0, len(height) - 1
+        left,right = 0, len(height) - 1
+        max_water = -1
         while left < right:
-            area = (right - left) * min(height[left],height[right])
-            res = max(res,area)
+            curr_height = min(height[left],height[right])
+            curr_width = right - left
+            curr_water = curr_height * curr_width
+            print(f"current water: {curr_water}")
+            max_water = max(max_water,curr_water)
             if height[left] < height[right]:
                 left += 1
-            elif height[left] > height[right]:
+            else: 
                 right -= 1
-            else:
-                right -= 1
+        return max_water
+        
+#         res = 0  
+#         left, right = 0, len(height) - 1
+#         while left < right:
+#             area = (right - left) * min(height[left],height[right])
+#             res = max(res,area)
+#             if height[left] < height[right]:
+#                 left += 1
+#             elif height[left] > height[right]:
+#                 right -= 1
+#             else:
+#                 right -= 1
                 
-        return res  
+#         return res  
         
 #         # init res 
 #         res = 0
