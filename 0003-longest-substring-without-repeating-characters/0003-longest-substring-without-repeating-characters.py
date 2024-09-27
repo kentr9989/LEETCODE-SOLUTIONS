28ab8e4c3a9c6d32093d70b57431ec09523d2a48
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-27 22:13:38 +1000

    Time: 45 ms (93.62%), Space: 16.7 MB (9.57%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,30 +1,61 @@
 class Solution:
     # Time complexity: O(n)
-    # Space complexity: O(n)
+    # Space complexity: O(n) - from the set
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # implement new set char_set
+        left, right = 0, 0
+        longest_substr = 0
         char_set = set()
         
-        # init res equal 0
-        res = 0
-        # init left pointer 0
-        left = 0
-        
-        # for right in range (len(s)):
-        #   while character on the right is in char_set
-        #      remove the most left character from char_set
-        #      increase the left pointer
-        #   add right most character to char_set
-        #   update our res for the max substring
-        #   between res and (r-l+1)
-        for right in range(len(s)):
+        while right < len(s):
             while s[right] in char_set:
                 char_set.remove(s[left])
                 left += 1
+            curr_substr = right - left + 1
+            longest_substr = max(curr_substr,longest_substr)
             char_set.add(s[right])
-            res = max(res, (right - left + 1))
+            right += 1
+            
+        return longest_substr 
+        
+        
+#         char_set = set()
+        
+#         left = 0
+#         res = 0
+        
+#         for right in range(len(s)):
+#             while s[right] in char_set:
+#                 char_set.remove(s[left])
+#                 left += 1
+#             res = max((right - left + 1), res)
+#             char_set.add(s[right])
+#         return res
+    
+        
+        
+        
+#         # implement new set char_set
+#         char_set = set()
+        
+#         # init res equal 0
+#         res = 0
+#         # init left pointer 0
+#         left = 0
+        
+#         # for right in range (len(s)):
+#         #   while character on the right is in char_set
+#         #      remove the most left character from char_set
+#         #      increase the left pointer
+#         #   add right most character to char_set
+#         #   update our res for the max substring between res and (r-l+1)
+#         for right in range(len(s)):
+#             while s[right] in char_set:
+#                 char_set.remove(s[left])
+#                 left += 1
+#             char_set.add(s[right])
+#             res = max(res, (right - left + 1))
             
         
         
-        # return res
-        return res
+#         # return res
+#         return res
