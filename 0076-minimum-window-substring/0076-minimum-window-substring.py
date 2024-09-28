8ea8754ac0f03583650c881889d11d8d282b7646
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-28 17:50:07 +1000

    Time: 108 ms (70.64%), Space: 17.3 MB (25.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,68 +2,93 @@ class Solution:
     # Time complexity: O(n)
     # Space complexity: O(n) - length of s
     def minWindow(self, s: str, t: str) -> str:
-        # if t is empty string:
-        #   return the empty string
-        if t == "": 
+        if t == "":
             return ""
-        
-        # init map for s {character : count}
         map_s = {}
-        # init map for t {character : count}
         map_t = {}
-        # inject all value for map t
-        for char in t:
-            map_t[char] = 1 + map_t.get(char,0)
         
-        # init have is equal 0
-        have = 0
-        # init need as length of t
-        need = len(map_t)
-        # init res to be start and end index
-        res = [-1,-1]
-        # init res length to be infinity
-        resLen = float("infinity")
-        # init left pointer equal 0
-        left = 0
-        # for right pointer (0 -> length of s):
-        #   add the character of s[right] to map of s
-        #   check if s[right] is in map of t AND
-        #   s[right] equal to map_t[s[right]]:
-        #       increase have by 1
-        #   while have == need:
-        #       Update the new res and resLen
-        #       remove s[left] from map_s
-        #       check if s[left] is in map_t[] AND
-        #       map_s[s[left]] < map_t[s[left]]:
-        #           decrease have by 1
-        #       increase left pointer by 1
-        for right in range(len(s)):
-            map_s[s[right]] = 1 + map_s.get(s[right],0)
+        # fill up map_t frequency
+        for char_t in t:
+            map_t[char_t] = map_t.get(char_t,0) + 1
             
-            if s[right] in map_t and map_s[s[right]] == map_t[s[right]]:
-                have += 1
-                
-            while have == need:
-                if (right - left + 1) < resLen:
-                    res = [left,right]
-                    resLen = right - left + 1
-                map_s[s[left]] -= 1
-                if s[left] in map_t and map_s[s[left]] < map_t[s[left]]:
-                    have -= 1
-                left += 1
+        # print(f"map_t: {map_t}")
+        have,need = 0, len(map_t)
+        # print(f"need: {need}")
+         
         
-        # res = [left,right]    
-        #  if resLen is still infinity:
-        #       return "" - nothing is found
-        #  else:
-        #       return the string from pos left to right + 1 (slicing)
-        if resLen != float("infinity"):
-            return s[res[0]:res[1]+1]
-        else:
+        resLen = float("infinity")
+        res = [-1,-1]
+        l = 0
+        
+        for r in range(len(s)):
+            map_s[s[r]] = map_s.get(s[r],0) + 1
+            if s[r] in map_t and map_s[s[r]] == map_t[s[r]]:
+                have += 1
+            # print(f"have: {have}")
+            # print(f"map_s: {map_s}")    
+            # print(f"left: {l}")
+            while have == need:
+                if resLen > r - l + 1:
+                    resLen = r - l + 1
+                    res = [l,r]
+                
+                map_s[s[l]] -= 1
+                # if map_s[s[l]] == 0: 
+                #     del map_s[s[l]]    
+                if s[l] in map_t and map_t[s[l]] > map_s[s[l]]:
+                    have -= 1
+                l += 1
+#                 print(f"resLen: {resLen}")
+#                 print(f"res: {res}")
+        
+        if resLen == float("infinity"):
             return ""
-       
-    
-    
+            
+        return s[res[0]:res[1] + 1]
+        
+        
+        
+        
+#         if t == "":
+#             return ""
+#         map_s = {}
+#         map_t = {}
+        
+#         for st in t:
+#             map_t[st] = map_t.get(st,0) + 1
+#         # print(map_t)
+#         have, need = 0, len(map_t)
+#         resLen = float("infinity")
+#         res = [-1,-1]
+#         left = 0
+        
+#         for right in range(len(s)):
+#             map_s[s[right]] = map_s.get(s[right],0) + 1
+#             # print("map_s is" + str(map_s))
+#             if s[right] in map_t and map_s[s[right]] == map_t[s[right]]:
+#                 # print("map_s is" + str(map_s))
+#                 # print("have is" + str(have))
+#                 have += 1
+           
+#             # print("have is" + str(have))
+#             while have == need:
+#                 if (right - left + 1) < resLen:
+#                     res = [left,right]
+#                     # print("res is" + str(res))
+#                     resLen = right - left + 1
+#                 map_s[s[left]] -= 1
+#                 if s[left] in map_t and map_s[s[left]] < map_t[s[left]]:
+#                     have -= 1
+#                 # print("res is" + str(res))
+#                 left += 1 
+# #                 print(res)
+               
+                
+#         if resLen != float("infinity"):
+#             return s[res[0]:res[1] + 1]
+#         else:
+#             return ""
+
     
     
     
