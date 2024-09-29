51a55db3f004a820d5b79e6a29755deb30253d6b
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-29 21:21:23 +1000

    Time: 51 ms (19.05%), Space: 17.2 MB (23.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -6,82 +6,72 @@ class Solution:
         COLS = len(matrix[0])
         
         top, bot = 0 , ROWS - 1
+        
         while top <= bot:
             mid = (top + bot) // 2
             if matrix[mid][-1] < target:
-                top = mid + 1
+                top += 1
             elif matrix[mid][0] > target:
-                bot = mid - 1
+                bot -= 1
             else:
                 break
+        print(f"top: {top}")
+        print(f"bot: {bot}")
         if top > bot:
+            print(f"top: {top}")
             return False
-        
         row = (top + bot) // 2
-        left , right = 0, COLS - 1
-        
+        left, right = 0 , COLS - 1
         while left <= right:
             mid = (left + right) // 2
             if matrix[row][mid] < target:
-                left = mid + 1
+                left += 1
             elif matrix[row][mid] > target:
-                right = mid - 1
+                right -= 1
             else:
                 return True
-        return False
-#         # ROWS = len of matrix
-#         ROWS = len(matrix)
-#         # COLS = len of matrix[0]
-#         COLS = len(matrix[0])
-                
-#         # init top = 0
-#         top = 0
-#         # init bottom ROWS -1
-#         bot = ROWS - 1
         
-#         # while top <= bottom:
-#         #   middle = (top + bot) // 2
-#         #   if target greater than matrix[middle][-1] - greatest value in the row:
-#         #       top = middle + 1
-#         #   else if targer < matrix[middle][0] - smallest value in the row:
-#         #       bottom = middle - 1
-#         #   else:
-#         #       break
+        return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ROWS = len(matrix)
+#         COLS = len(matrix[0])
+        
+#         top, bot = 0 , ROWS - 1
 #         while top <= bot:
 #             mid = (top + bot) // 2
-#             if target > matrix[mid][-1]:
+#             if matrix[mid][-1] < target:
 #                 top = mid + 1
-#             elif target < matrix[mid][0]:
+#             elif matrix[mid][0] > target:
 #                 bot = mid - 1
-#             else: 
+#             else:
 #                 break
-        
-#         # if top > bottom:
-#         #    return False
 #         if top > bot:
 #             return False
         
-#         # row = (top + bottom) // 2
-#         # l,r = 0 , COL -1
-#         # while left <= right:
-#         #   mid = (left + right) // 2
-#         #   if (target > matrix[row][mid]):
-#         #       left = mid + 1
-#         #   else if targer < matrix[row][mid]:
-#         #       right = mid + 1
-#         #   else:
-#         #       return True
 #         row = (top + bot) // 2
-#         left = 0
-#         right = COLS - 1
+#         left , right = 0, COLS - 1
+        
 #         while left <= right:
 #             mid = (left + right) // 2
-#             if target > matrix[row][mid]:
+#             if matrix[row][mid] < target:
 #                 left = mid + 1
-#             elif target < matrix[row][mid]:
+#             elif matrix[row][mid] > target:
 #                 right = mid - 1
 #             else:
 #                 return True
-        
-#         # return False
 #         return False
+    
+    
+    
