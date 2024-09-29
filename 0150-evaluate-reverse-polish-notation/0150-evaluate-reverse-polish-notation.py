ebd40c7a24c1d3a69b90241a7bf03325cb7c205c
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-29 12:03:54 +1000

    Time: 50 ms (99.59%), Space: 17 MB (63.36%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -2,35 +2,88 @@ class Solution:
     # Time complexity: O(n)
     # Space complexity: O(n)
     def evalRPN(self, tokens: List[str]) -> int:
-        # Init stack
         stack = []
-        # loop through every character in token:
-        #   if character is "+":
-        #       pop from our stack twice then add tgt
-        #       append result to the stack
-        #   else if character is "-":
-        #       pop from stack twice then deduct
-        #       second number - first number
-        #   else if character is "*":
-        #   else if character is "/":
-        #   else: 
-        #       stack append the number (conver to int)
-        
-        # return top of the stack
-        for char in tokens:
-            if char == "+":
-                stack.append(stack.pop()+stack.pop())
-            elif char == "-":
+        for token in tokens:
+            if token == "+":
+                first,second = stack.pop(), stack.pop()
+                stack.append(first + second)
+            elif token == "-":
                 first,second = stack.pop(), stack.pop()
                 stack.append(second - first)
-            elif char == "*":
-                stack.append(stack.pop() * stack.pop())
-            elif char == "/":
+            elif token == "*":
                 first,second = stack.pop(), stack.pop()
-                stack.append(int(second/first)) # convert to 0
+                stack.append(first * second)
+            elif token == "/":
+                first,second = stack.pop(),stack.pop()
+                stack.append(int(second / first))
             else:
-                stack.append(int(char))
+                stack.append(int(token))
+            # print(f"stack: {stack}")
+            
         return stack[0]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         stack = []
+#         for char in tokens:
+#             if char == "+":
+#                 first,second = stack.pop(), stack.pop()
+#                 stack.append(first + second)
+#             elif char == "-":
+#                 first,second = stack.pop(),stack.pop()
+#                 stack.append(second - first)
+#             elif char == "*":
+#                 first, second = stack.pop(), stack.pop()
+#                 stack.append(second * first)
+#             elif char == '/':
+#                 first, second = stack.pop(), stack.pop()
+#                 stack.append(int(second / first))
+#             else:
+#                 stack.append(int(char))
+        
+#         return stack[0]
+                
+                
+#         # Init stack
+#         stack = []
+#         # loop through every character in token:
+#         #   if character is "+":
+#         #       pop from our stack twice then add tgt
+#         #       append result to the stack
+#         #   else if character is "-":
+#         #       pop from stack twice then deduct
+#         #       second number - first number
+#         #   else if character is "*":
+#         #   else if character is "/":
+#         #   else: 
+#         #       stack append the number (conver to int)
+        
+#         # return top of the stack
+#         for char in tokens:
+#             if char == "+":
+#                 stack.append(stack.pop()+stack.pop())
+#             elif char == "-":
+#                 first,second = stack.pop(), stack.pop()
+#                 stack.append(second - first)
+#             elif char == "*":
+#                 stack.append(stack.pop() * stack.pop())
+#             elif char == "/":
+#                 first,second = stack.pop(), stack.pop()
+#                 stack.append(int(second/first)) # convert to 0
+#             else:
+#                 stack.append(int(char))
+#         return stack[0]
                 
                 
                 
