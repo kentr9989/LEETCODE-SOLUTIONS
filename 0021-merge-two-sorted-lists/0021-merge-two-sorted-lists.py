3ed34bf748be8bc75f98095ac6ace645522a6aa2
Peter <133621347+kentr9989@users.noreply.github.com>
2024-10-01 00:55:33 +1000

    Time: 39 ms (49.47%), Space: 16.6 MB (37.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,7 +7,6 @@ class Solution:
     # Time complexity: O(l1 + l2)
     # Space complexity: O(1)
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
         dummy = ListNode()
         tail = dummy
         
@@ -19,50 +18,37 @@ class Solution:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
+        
         if list1:
             tail.next = list1
-        elif list2:
+        if list2:
             tail.next = list2
         return dummy.next
-    
-#         # initially it look like this
-#         # [dummy]/[tail]
-#         # then later on
-#         # [dummy] -> 1->2-> 3
-#         #                   ^ (where tail is)
-#         # so dummy->next will be [1]
         
-#         # create dummy node
+        
+        
+        
+        
+        
+        
+        
+        
 #         dummy = ListNode()
-#         # init tail as dummy
 #         tail = dummy
-#         # while l1 and l2 is not null:
-#         #   if current value of l1 < current value of l2:
-#         #       tail.next = l1
-#         #       l1 = l1.next
-#         #   else:
-#         #       tail.next = l2
-#         #       l2 = l2.next
-#         #   tail equal tail.next
+        
 #         while list1 and list2:
 #             if list1.val < list2.val:
 #                 tail.next = list1
 #                 list1 = list1.next
-#             else: 
+#             else:
 #                 tail.next = list2
 #                 list2 = list2.next
 #             tail = tail.next
-#         # find non empty list
-#         # if l1 not null:
-#         #   tail.next = l1
-#         # else if l2 not null: 
-#         #   tail.next = l2
 #         if list1:
 #             tail.next = list1
 #         elif list2:
 #             tail.next = list2
-            
-#         # return dummy.next
 #         return dummy.next
+
         
         
