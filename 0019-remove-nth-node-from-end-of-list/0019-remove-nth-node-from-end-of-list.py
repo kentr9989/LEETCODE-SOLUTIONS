5a0f4694b0de2ddb6c10cb3358a20d23befcd827
Peter <133621347+kentr9989@users.noreply.github.com>
2024-10-01 01:32:05 +1000

    Time: 33 ms (78.25%), Space: 16.6 MB (25.66%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -6,34 +6,41 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         dummy = ListNode(0,head)
-        left,right = dummy, head
+        left, right = dummy, head
+        
         while n > 0 and right:
-            right = right.next
             n -= 1
+            right = right.next
+        # print(f"right: {right}")
+        
         while right:
             left = left.next
             right = right.next
+        print(f"left: {left}")    
+        print(f"right: {right}")
+        
         left.next = left.next.next
         return dummy.next
-#         # create dummy node to head
-#         dummy = ListNode(0,head)
-#         # init left pointer to dummy
-#         left = dummy
-#         # init right pointer to head + n (use loop)
-#         right = head
-#         while n > 0 and right:
-#             right = right.next
-#             n -= 1
-#         # while right is not null:
-#         #   shift left = left.next
-#         #   right = right.next
-#         while right:
-#             left = left.next
-#             right = right.next
-            
-#         # delete the node
-#         # left.next = left.next.next
-#         left.next = left.next.next
         
-#         # return dummy.next
-#         return dummy.next
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # dummy = ListNode(0,head)
+        # left,right = dummy, head
+        # while n > 0 and right:
+        #     right = right.next
+        #     n -= 1
+        # while right:
+        #     left = left.next
+        #     right = right.next
+        # left.next = left.next.next
+        # return dummy.next
