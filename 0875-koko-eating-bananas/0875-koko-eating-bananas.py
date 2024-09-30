bef9fc5bd2393940cfc3592caf577db26e833b6c
Peter <133621347+kentr9989@users.noreply.github.com>
2024-09-30 11:36:00 +1000

    Time: 259 ms (76.38%), Space: 18.2 MB (17.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,6 +1,6 @@
 class Solution:
-    # Time complexity: O(log(max(piles)*piles))
-    # Space complexity: O(max(piles))
+    # Time Complexity: O(n * log M) where n is the number of piles, and M is the maximum pile size.
+    # Space Complexity: O(1) (constant space).
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left = 1
         right = max(piles)
@@ -13,42 +13,33 @@ class Solution:
                 hours += math.ceil(pile/mid)
             if hours <= h:
                 res = mid
-                right = mid - 1
+                right = mid - 1 
             else:
                 left = mid + 1
-        
         return res
         
         
         
-#         # left is equal 1 (bc need to eat least 1 banana)
-#         left = 1
-#         # right is equal max(piles) - last index
-#         right = max(piles) # piles[-1]
-#         # res = right
-#         res = right
-#         # while left is less than or qual right:
-#         #   mid = left + right / 2
-#         #   hours = 0
-#         #   go to every pile:
-#         #       hours += math.ceil(pile/mid)
         
-#         #   if hours (mid) <= h (target):
-#         #       update res
-#         #       right = mid - 1 (search left portion)
-#         #   else:
-#         #       left = mid + 1 (search for right portion)
+        
+        
+        
+        
+        
+#         left = 1
+#         right = max(piles)
+#         res = right
+        
 #         while left <= right:
 #             mid = (left + right) // 2
 #             hours = 0
 #             for pile in piles:
 #                 hours += math.ceil(pile/mid)
-            
 #             if hours <= h:
-#                 res = min(mid,res)
+#                 res = mid
 #                 right = mid - 1
 #             else:
 #                 left = mid + 1
         
-#         # return res
 #         return res
+        
