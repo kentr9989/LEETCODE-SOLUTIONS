2f50b7fd733dbd67c54313653b1bb6354e36c7a8
Peter <133621347+kentr9989@users.noreply.github.com>
2024-10-03 14:16:42 +1000

    Time: 42 ms (98.56%), Space: 20.3 MB (37.70%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -9,21 +9,31 @@ class Solution:
     # Time complexity : O(n)
     # Space complexity: O(1)
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # start at root node assign to cur
         curr = root
-        # while curr is not null:
-        #   if value of p > value of cur and value of q > value of cur:
-        #       cur = cur.right
-        #   else if value of p < value of cur and value of p < value of cur:
-        #       cur = cur.left
-        #   else:
-        #       return cur
+        
         while curr:
-            if p.val > curr.val and q.val > curr.val:
+            if curr.val < p.val and curr.val < q.val:
                 curr = curr.right
-            elif p.val < curr.val and q.val < curr.val:
+            elif curr.val > p.val and curr.val > q.val:
                 curr = curr.left
             else:
                 return curr
+        
+        
+        
+        
+        
+        
+        
+        
+        # # start at root node assign to cur
+        # curr = root
+        # while curr:
+        #     if p.val > curr.val and q.val > curr.val:
+        #         curr = curr.right
+        #     elif p.val < curr.val and q.val < curr.val:
+        #         curr = curr.left
+        #     else:
+        #         return curr
         
         
