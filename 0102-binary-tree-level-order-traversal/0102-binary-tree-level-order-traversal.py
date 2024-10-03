8d7d864d35f1c574b09dfcfc75a510e34c7bede2
Peter <133621347+kentr9989@users.noreply.github.com>
2024-10-03 15:23:10 +1000

    Time: 33 ms (91.43%), Space: 17.2 MB (73.10%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -8,39 +8,66 @@ class Solution:
     # Time complexity: O(n)
     # Space complexity: O(n) queue at any given point have up to n/2 elements
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # if root is empty return []
         if not root:
             return []
-        # init queue first element of [root]
-        queue = collections.deque()
-        queue.append(root)
-        
-        # create res with []
         res = []
-        # while queue is not empty:
-        #   create res1 to be []
-        #   for i in range (0 to len(q)):
-        #       node = pop left element
-        #       if node 
-        #           add to res1
-        #           append node.left to queue
-        #           append node.right to queue
-        #               append to queue
-        #       if res1 is not empty:
-        #           add res1 to res
-        while queue:
-            res1 = []
-            for i in range(len(queue)):
-                node = queue.popleft()
-                if node:
-                    res1.append(node.val)
+        q = deque([root])
+        res.append([root.val])
+        while q:
+            temp_arr = []
+            for i in range(len(q)):
+                node = q.popleft()
+                # print(f"node: {node}")
                 if node.left:
-                    queue.append(node.left)  # append left child to queue
+                    q.append(node.left)
+                    temp_arr.append(node.left.val)
                 if node.right:
-                    queue.append(node.right)  # append right child to queue
-            if res1:
-                res.append(res1)
+                    q.append(node.right)
+                    temp_arr.append(node.right.val)
+            res.append(temp_arr) if len(temp_arr) > 0 else None
+            # print(res)
+        return res   
+        
+        
+        
+        
+        
+        
+        
+        
+#         # if root is empty return []
+#         if not root:
+#             return []
+#         # init queue first element of [root]
+#         queue = collections.deque()
+#         queue.append(root)
+        
+#         # create res with []
+#         res = []
+#         # while queue is not empty:
+#         #   create res1 to be []
+#         #   for i in range (0 to len(q)):
+#         #       node = pop left element
+#         #       if node 
+#         #           add to res1
+#         #           append node.left to queue
+#         #           append node.right to queue
+#         #               append to queue
+#         #       if res1 is not empty:
+#         #           add res1 to res
+#         while queue:
+#             res1 = []
+#             for i in range(len(queue)):
+#                 node = queue.popleft()
+#                 if node:
+#                     res1.append(node.val)
+#                 if node.left:
+#                     queue.append(node.left)  # append left child to queue
+#                 if node.right:
+#                     queue.append(node.right)  # append right child to queue
+#             if res1:
+#                 res.append(res1)
                     
         
-        #  return res
-        return res
+#         #  return res
+#         return res
