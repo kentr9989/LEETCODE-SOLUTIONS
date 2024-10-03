3597d62fe3905d0ffc359ca53a9d25b8a906afe4
Peter <133621347+kentr9989@users.noreply.github.com>
2024-10-03 21:01:40 +1000

    Time: 47 ms (19.37%), Space: 18.5 MB (22.55%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,21 +6,49 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # create function to traverse dfs
-        def valid(node, low , high):
-            # if node is null:
-            #   return true
-            if not node:
+        
+        
+        
+        def valid(curr, low ,high) :
+            if not curr:
                 return True
-            # if value is not between low and high:
-            #   return false
-            if not (low < node.val < high):
-                return False
-            # return (node.left, low, node.val) AND
-            #        (node.right, node.val, high)               
-            return (valid(node.left,low,node.val) and
-                   valid(node.right,node.val,high))
             
-        # return call the valid
-        return valid(root,float("-inf"),float("inf"))
+            if not (curr.val > low and curr.val < high) :
+              return False
+            
+            return valid(curr.left,low,curr.val) and valid(curr.right,curr.val,high)
+        
+        return valid(root,float("-infinity"), float("infinity"))
+        
+        # (2, -inf , inf)
+        # valid(1,-inf,2)  -> True           and valid(3,2,inf)
+        # valid(null, -inf, 1) and val(null,1,inf) -> True
+        #    True and True
+    #     2
+    #    / \
+    #   2    2      
+    #  / \  / \
+    # n   n 3  6
+        
+        
+        
+        
+        
+#         # create function to traverse dfs
+#         def valid(node, low , high):
+#             # if node is null:
+#             #   return true
+#             if not node:
+#                 return True
+#             # if value is not between low and high:
+#             #   return false
+#             if not (low < node.val < high):
+#                 return False
+#             # return (node.left, low, node.val) AND
+#             #        (node.right, node.val, high)               
+#             return (valid(node.left,low,node.val) and
+#                    valid(node.right,node.val,high))
+            
+#         # return call the valid
+#         return valid(root,float("-inf"),float("inf"))
         
