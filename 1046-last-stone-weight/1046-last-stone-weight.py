0a94b83cccc6466cd33c986c804a43adb9f8e241
Peter <133621347+kentr9989@users.noreply.github.com>
2024-10-04 21:15:17 +1000

    Time: 38 ms (41.84%), Space: 16.7 MB (6.85%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,26 +1,46 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        if len(stones) == 1: return stones[0]
+        copy_stone = [-s for s in stones]
+        heapq.heapify(copy_stone)
+        print(copy_stone)
+        while len(copy_stone) > 1:
+            first,second = -heapq.heappop(copy_stone), -heapq.heappop(copy_stone)
+            print(f"first: {first}")
+            print(f"second: {second}")
+            if first > second:
+                diff = (first - second) * (-1) 
+                heapq.heappush(copy_stone,diff)
+            print(copy_stone)
+        print(copy_stone)
+        return abs(copy_stone[0]) if len(copy_stone) > 0 else 0;
         
-        heapq.heapify(stones)
-        # print(stones)
-        # print(stones[-1])
-        # print(stones[-2])
-        # print(min_heap)
-        # two_largest = heapq.nlargest(2,stones)
-        # print(two_largest)
-        while len(stones) > 1:
-            two_largest = heapq.nlargest(2,stones)
-            diff = two_largest[0] - two_largest[1]
-            stones.remove(two_largest[0])
-            stones.remove(two_largest[1])
-            heapq.heapify(stones)
-            heapq.heappush(stones,diff)
+        
+        # [4,2]
+        # 2
+        
+        
+        
+        
+        
+        
+        
+#         Time complexity: O(n^2)
+#         Space complexity: O(1)
+#         if len(stones) == 1: return stones[0]
+        
+#         heapq.heapify(stones)
+#         while len(stones) > 1:
+#             two_largest = heapq.nlargest(2,stones)
+#             diff = two_largest[0] - two_largest[1]
+#             stones.remove(two_largest[0])
+#             stones.remove(two_largest[1])
+#             heapq.heapify(stones)
+#             heapq.heappush(stones,diff)
             
             
         
         
-        return stones[0]
+#         return stones[0]
         
         
         # [1,1,2,4,7,8]
