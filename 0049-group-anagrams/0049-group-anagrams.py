63ce48c4092e6c7e0230ac9cea280f6b82ede5c5
Peter <133621347+kentr9989@users.noreply.github.com>
2024-12-01 22:28:42 +1100

    Time: 11 ms (79.96%), Space: 20.2 MB (48.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,4 +1,8 @@
 class Solution:
+    # n : number of strings
+    # k : avg length of each string
+    # Time complexity: O(nlogk)
+    # Space complexity: O(nk)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # map : {key : sorted_str , value: array of original value}
         map = {}
@@ -19,64 +23,7 @@ class Solution:
             
         return res_arr
         
-        
-        
-        
-        
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            #     # Time complexity : O(length of strs * length of each str * log(length of each str))
-#     # Space complexity: O(length of strs * length of each str)
-        
-#         # make a map {key : sorted , value : array of original str}
-#         map = {}
-#         for str in strs:
-#             sort_str = ''.join(sorted(str))
-#             if sort_str not in map:
-#                 map[sort_str] = []
-#             map[sort_str].append(str)
-#         print(map)
-#         return list(map.values())
-        
-# if __name__ == "__main__":
-#     solution = Solution()
-#     result = solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
-#     print(f" Result: {result}")
 
-
-
-
-# class Solution:
-#     # Time complexity : O(length of strs * length of each str * log(length of each str))
-#     # Space complexity: O(length of strs * length of each str)
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         # Create a map to store sortedStr : array of real value
-#         map = {}
-#         # For each str in strs:
-#         #   sort the str by alphabetical order
-#         #   if the sort str is not in map:
-#         #       add the sort string to the map as key and value is []
-#         #   else:
-#         #       add the str (NOT SORTED) to the map value
-#         for str in strs:
-#             sort_str = ''.join(sorted(str))
-#             if sort_str not in map:
-#                 map[sort_str] = []
-#             map[sort_str].append(str)
-        
-#         # return the array from value from map
-#         return list(map.values())
         
 
         
