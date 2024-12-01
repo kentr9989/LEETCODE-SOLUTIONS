afdab9c60f9c6387dd5f6797a0764cd8a1e4ef53
Peter <133621347+kentr9989@users.noreply.github.com>
2024-12-01 23:05:15 +1100

    Time: 14 ms (12.22%), Space: 24.1 MB (5.87%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,35 +1,74 @@
 class Solution:
     def topKFrequent(self,nums: List[int], k:int) -> List[int]:
-        # map to store frequency
-        map_freq = {}
+        # map element by its frequency {key: num , value : freq}
+        freq_map = {}
         for num in nums:
-            if num in nums:
-                map_freq[num] = map_freq.get(num,0) + 1
-        print(f"Map freq: {map_freq}")
+            if num not in freq_map:
+                freq_map[num] = 0
+            freq_map[num] += 1
+        print(freq_map)
+        # bucket sort arr : {index: freq, value: element with same freq}
+        bucket_arr = [[] for i in range(len(nums) + 1)]
+        print(bucket_arr)
+        for item,value in freq_map.items():
+            bucket_arr[value].append(item)
         
-        # map to store group frequency [arr[]]
-        map_group_freq = [[] for i in range(len(nums) + 1)]
-        for value,freq in map_freq.items():
-            print(value,freq)
-            map_group_freq[freq].append(value)
-        print(f"Map group freq: {map_group_freq}")
+        print(bucket_arr)
         
-        # return k element by looping through map_group_freq
-        res = []
-        count_k = 0
-        for freq in range(len(nums), 0 , -1):
-            print(f"Freq: {freq}")
-            for value in map_group_freq[freq]:
-                if count_k == k:
-                    return res
-                count_k += 1
-                res.append(value)
+        # start from bottom to top add to res_arr until k hit 0 
+        res_arr = []
+        for freq in range(len(nums),0,-1):
+            for value in bucket_arr[freq]:
+                res_arr.append(value)
+                k -= 1
+                if k == 0:
+                    return res_arr
+        
+        return []
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # map to store frequency
+#         map_freq = {}
+#         for num in nums:
+#             if num in nums:
+#                 map_freq[num] = map_freq.get(num,0) + 1
+#         print(f"Map freq: {map_freq}")
+        
+#         # map to store group frequency [arr[]]
+#         map_group_freq = [[] for i in range(len(nums) + 1)]
+#         for value,freq in map_freq.items():
+#             print(value,freq)
+#             map_group_freq[freq].append(value)
+#         print(f"Map group freq: {map_group_freq}")
+        
+#         # return k element by looping through map_group_freq
+#         res = []
+#         count_k = 0
+#         for freq in range(len(nums), 0 , -1):
+#             print(f"Freq: {freq}")
+#             for value in map_group_freq[freq]:
+#                 if count_k == k:
+#                     return res
+#                 count_k += 1
+#                 res.append(value)
                 
-                # print(f" res: {res}")
-                # print(f" value: {value}")
+#                 # print(f" res: {res}")
+#                 # print(f" value: {value}")
         
        
-        return res
+#         return res
 # if __name__ == "__main__":
 #     solution = Solution()
 #     result = solution.topKFrequent([1],1)
