fd8fcb16727fae0eb0ec2acb8219c1b3756b24f5
Peter <133621347+kentr9989@users.noreply.github.com>
2024-12-01 22:25:54 +1100

    Time: 7 ms (97.60%), Space: 20.2 MB (51.60%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,26 +1,82 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Create new dictionary : (sortedStr, value)
-        anagram_map = {}
-
-        # For each element in strs:
-        #    sort the element
-        #    if sorted_element in map:
-        #      add element to the value
-        #    else:
-        #      add sorted_element to map
-        for str in strs:
-            sorted_str = ''.join(sorted(str))
-            if sorted_str not in anagram_map:
-                anagram_map[sorted_str] = []
-            anagram_map[sorted_str].append(str)
+        # map : {key : sorted_str , value: array of original value}
+        map = {}
+        res_arr = []
         
-        # return the values in the map
-        return list(anagram_map.values())
-    
-    
-    
-    # Time complexity : O(n * klogk) where k is length for each         string
-    # Space complexity: O(n*k)
+        # push into map according to sorted str
+        for st in strs:
+            sorted_str = ''.join(sorted(st))
+            # print(f"sorted_str {sorted_str}")
+            if sorted_str not in map:
+                map[sorted_str] = []
+            map[sorted_str].append(st)
+        
+        for items,values in map.items():
+            res_arr.append(values)
+        # print(map)
+            
+            
+        return res_arr
+        
+        
+        
+        
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            #     # Time complexity : O(length of strs * length of each str * log(length of each str))
+#     # Space complexity: O(length of strs * length of each str)
+        
+#         # make a map {key : sorted , value : array of original str}
+#         map = {}
+#         for str in strs:
+#             sort_str = ''.join(sorted(str))
+#             if sort_str not in map:
+#                 map[sort_str] = []
+#             map[sort_str].append(str)
+#         print(map)
+#         return list(map.values())
+        
+# if __name__ == "__main__":
+#     solution = Solution()
+#     result = solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+#     print(f" Result: {result}")
+
+
+
+
+# class Solution:
+#     # Time complexity : O(length of strs * length of each str * log(length of each str))
+#     # Space complexity: O(length of strs * length of each str)
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         # Create a map to store sortedStr : array of real value
+#         map = {}
+#         # For each str in strs:
+#         #   sort the str by alphabetical order
+#         #   if the sort str is not in map:
+#         #       add the sort string to the map as key and value is []
+#         #   else:
+#         #       add the str (NOT SORTED) to the map value
+#         for str in strs:
+#             sort_str = ''.join(sorted(str))
+#             if sort_str not in map:
+#                 map[sort_str] = []
+#             map[sort_str].append(str)
+        
+#         # return the array from value from map
+#         return list(map.values())
+        
 
         
