17708f6f5152e3034a41f86b7a657d09011c3a5f
Peter <133621347+kentr9989@users.noreply.github.com>
2024-12-03 19:54:18 +1100

    Time: 598 ms (49.91%), Space: 21.1 MB (9.75%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,24 +2,71 @@ class Solution:
     # Time complexity : O(n^2)
     # Space complexity : O(1)
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
-        for index,num in enumerate(nums):
-            if index > 0 and nums[index - 1] == num:
-                continue
-            left, right = index + 1, len(nums) - 1
-            while left < right:
-                if num + nums[left] + nums[right] < 0:
-                    left += 1
-                elif num + nums[left] + nums[right] > 0:
-                    right -= 1
-                else:
-                    res.append([num,nums[left],nums[right]])
-                    left += 1
-                    while nums[left] == nums[left -1] and right > left:
-                        left += 1
         
-        return res
+        [-4,-1,-1,0,1,2]
+        [0,0,0,0]
+        # sort nums
+        nums.sort()
+        print(f"nums: {nums}")
+        # for each index,num:
+        #   init res array
+        #   find target
+        #   do binary search on index + 1 to len(nums) - 1
+        #   if target is found add to res array
+        res_arr = []
+        for index,num in enumerate(nums):
+            # if index >= 1:
+            
+            if index >= 1 and nums[index - 1] == num:
+                print(f"num - 1 : {nums[index - 1]} num: {num}")
+                continue
+            target = 0 - num
+            l, r = index + 1, len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] < target:
+                    l += 1
+                elif nums[l] + nums[r] > target:
+                    r -= 1
+                else:
+                    res_arr.append([num,nums[l],nums[r]])
+                    print( f"index: {index} left: {l} right: {r}")
+                    # while l < len(nums) - 1 and nums[l] == nums[l+1]:
+                    # while nums[l] == nums[l+1]:
+                    # break;
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                        print( f"2index: {index} left: {l} right: {r}")
+                    l += 1
+                    r -= 1 
+        return res_arr
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+#         nums.sort()
+#         for index,num in enumerate(nums):
+#             if index > 0 and nums[index - 1] == num:
+#                 continue
+#             left, right = index + 1, len(nums) - 1
+#             while left < right:
+#                 if num + nums[left] + nums[right] < 0:
+#                     left += 1
+#                 elif num + nums[left] + nums[right] > 0:
+#                     right -= 1
+#                 else:
+#                     res.append([num,nums[left],nums[right]])
+#                     left += 1 # new range of search
+#                     while nums[left] == nums[left -1] and right > left:
+#                         left += 1
+        
+#         return res
             
         
         
