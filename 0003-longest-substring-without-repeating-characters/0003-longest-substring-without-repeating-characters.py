87322fb234ba8e47571c582aae015d364152beed
Peter <133621347+kentr9989@users.noreply.github.com>
2024-12-04 00:18:27 +1100

    Time: 151 ms (12.06%), Space: 17.5 MB (5.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,20 +2,65 @@ class Solution:
     # Time complexity: O(n)
     # Space complexity: O(n) - from the set
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left, right = 0, 0
-        longest_substr = 0
-        char_set = set()
+        l, r = 0,0
+        map_char = {}
+        max_length = 0
         
-        while right < len(s):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
-            curr_substr = right - left + 1
-            longest_substr = max(curr_substr,longest_substr)
-            char_set.add(s[right])
-            right += 1
+        while r < len(s):
+            # print(f"1{s[l:r]}")
+            if s[r] not in map_char:
+                map_char[s[r]] = True
+                curr_length = r - l + 1
+                print(f"1curr_length: {curr_length}")
+                max_length = max(curr_length,max_length)
+                r += 1
+                print(f"1{s[l:r]}")
+            else:
+                while s[r] in map_char and l <= r:
+                    del map_char[s[l]]
+                    l += 1
+                    print(f"l: {l}")
+                    print(f"r: {r}")
+                    print(f"s: {s[l:r]}")
+                # l += 1
+                curr_length = r - l + 1
+                print(f"2curr_length: {curr_length}")
+                max_length = max(curr_length,max_length)
+                map_char[s[r]] = True
+                print(f"2{s[l:r]}")
+                r += 1
+
+        return max_length
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         left, right = 0, 0
+#         longest_substr = 0
+#         char_set = set()
+        
+#         while right < len(s):
+#             while s[right] in char_set:
+#                 char_set.remove(s[left])
+#                 left += 1
+#             curr_substr = right - left + 1
+#             longest_substr = max(curr_substr,longest_substr)
+#             char_set.add(s[right])
+#             right += 1
             
-        return longest_substr 
+#         return longest_substr 
         
         
 #         char_set = set()
